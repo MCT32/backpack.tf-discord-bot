1e189404916c19a26477c6962d68ab711d4320f7
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-12-15 19:05:06 +1100

    made bot list servers

--- bot.js
+++ bot.js
@@ -1,8 +1,19 @@
-var fs = require('fs');
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require('fs')
 
-var token;
+var token
 
 fs.readFile('token.txt', 'utf8', function(err, data) {
         if (err) throw err;
         token = data;
-});
+})
+
+client.on('ready', () => {
+        console.log('Servers:')
+        client.guilds.forEach((guild) => {
+                console.log(' - ' + guild.name)
+        })
+})
+
+client.login(token)
