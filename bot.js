8f419355fe9fae375a918d8814447e2c075dadb9
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-12-15 19:26:23 +1100

    made bot list server names

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const token = fs.readFileSync('token.txt').toString()
 const client = new Discord.Client()
 
 client.on('ready', () => {
-        const Guilds = client.guilds.cache.map(guild => guild.id)
+        const Guilds = client.guilds.cache.map(guild => guild.name)
         console.log('Servers:')
         console.log(Guilds)
 })
