73b64cbdbe8e11de797fc1ea0b1a14a1c088553c
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-12-16 15:22:22 +1100

    changed colors to variables and started tryang to add arguments to the parser

--- bot.js
+++ bot.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const token = fs.readFileSync('token.txt').toString()
 const client = new Discord.Client()
 
+const red = '#dc322f'
+const blue = '#268bd2'
+
 client.on('ready', () => {
         const Guilds = client.guilds.cache.map(guild => guild.name)
         console.log('Servers:')
@@ -12,7 +15,7 @@ client.on('ready', () => {
 
 global.help = function help(message, argc, argv) {
         const embed = new Discord.MessageEmbed()
-                .setColor('#268bd2')
+                .setColor(blue)
                 .setTitle('List of commands')
                 .setDescription('Here are all the commands you can use with this bot. (All start with \'?\')')
                 .addField('Commands',
@@ -21,20 +24,48 @@ global.help = function help(message, argc, argv) {
         message.channel.send(embed)
 }
 
+global.test = function test(message, argc, argv) {
+        if (argc > 0) {
+                const embed = new Discord.MessageEmbed()
+                        .setColor(blue)
+                        .setTitle('You said')
+                        .setDescription(argv)
+                //console.log(argv[0] + '\n' + argv[1])
+
+                message.channel.send(embed)
+        } else {
+                usage()
+        }
+
+        function usage() {
+                const embed = new Discord.MessageEmbed()
+                        .setColor(red)
+                        .setTitle('Correct syntax')
+                        .setDescription('`?test <arg1> [arg2]...`')
+
+                message.channel.send(embed)
+        }
+}
+
 client.on('message', (message) => {
         if (message.toString().startsWith('?')) {
                 console.log('message received!')
-                var command = message.toString().substring(1)
-                console.log(command)
+                var content = message.toString().substring(1)
+                var command = content.split(' ')[0]
+                //console.log(command)
 
                 if (command in global && typeof global[command] === 'function') {
-                        var argv = command.split(' ').shift()
+                        var argv = content.split(' ')
+                        Object.values(argv)
+                        console.log(typeof argv)
+                        console.log(typeof argv[0])
+                        content.shift()
                         var argc = argv.length
 
                         global[command](message, argc, argv);
                 } else {
                         const embed = new Discord.MessageEmbed()
-                                .setColor('#dc322f')
+                                .setColor(red)
                                 .setTitle('Command not found')
                                 .setDescription('The command you typed does not exist.')
 
